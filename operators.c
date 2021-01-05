81822a116accb5fda9ddfde60fd8ac19696738e7
prakshatandonkiet <75250776+prakshatandonkiet@users.noreply.github.com>
2021-01-05 05:21:01 -0800

    Add files via upload

---
+++ operators.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+    int num1=20;
+    int num2=30;
+    int sum=num1+num2;
+    int min=num2-num1;
+    printf("\n Sum of two number=%d",sum);
+    printf("\n Substraction of two number=%d",min);
+    printf("\n num1 is greater than num2");
+    if (num1<num2)
+    {
+        printf("false");
+
+    }   
+    else
+    {
+        printf("true");
+
+    }
+    
+
+
+
+    return 0;
+}
