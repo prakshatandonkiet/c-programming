b2f32c138c1ef6df5ecad163216fc9e4fd078ced
prakshatandonkiet <75250776+prakshatandonkiet@users.noreply.github.com>
2021-01-06 05:08:51 -0800

    Add files via upload

---
+++ seventh.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    int num1,num2;
+    
+    printf("\nEnter value of num1 and num2");
+    //Store input values in variable for comaprision
+    scanf("%d %d",&num1,&num2);
+    if(num1>num2)
+ 
+     printf("\n Number1 is greater");
+
+ 
+ else(num2>num1);
+ 
+     printf("\n Number2 is grater");
+ 
+ return 0;
+ }
+   
+
+
+    
+
+
