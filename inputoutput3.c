f5d4f60db5260ea4840226e5def0e38e433f1bbe
prakshatandonkiet <75250776+prakshatandonkiet@users.noreply.github.com>
2021-01-12 05:03:03 -0800

    Add files via upload

---
+++ inputoutput3.c
@@ -0,0 +1,13 @@
+               #include<stdio.h>
+	int main( )
+    {
+
+	char str[50];
+                  printf("enter your sentence:");
+	     gets(str);
+
+	printf("Your sentence is =>\n%s",str);
+
+	return 0;
+
+}
