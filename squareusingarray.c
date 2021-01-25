db57bfb186b8e9119f207352a4fe3cd000e0da48
prakshatandonkiet <75250776+prakshatandonkiet@users.noreply.github.com>
2021-01-25 05:41:18 -0800

    Add files via upload

---
+++ squareusingarray.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(int argc, char const *argv[])
+{
+    int square[11],i;
+    for(i=0;i<11;i++)
+    {
+        square[i]=i*i;
+        printf("\n square[%d] %d",i,square[i]);
+    }
+    return 0;
+}
