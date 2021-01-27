545cd1119e8274b35f03e5c1b05fc074d4b1c884
prakshatandonkiet <75250776+prakshatandonkiet@users.noreply.github.com>
2021-01-27 04:46:26 -0800

    Add files via upload

---
+++ conditiontional.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main()
+{
+    int a,b;
+    printf("\n Enter the value of the variable\"a\":");
+    scanf("%d",&a);
+    b=a>35?100:90;
+    printf("\n The value of variable \"b\" is %d",b);
+}
