20c4a52e1f27104e6ae2db0e93d28e4b8528f29f
prakshatandonkiet <75250776+prakshatandonkiet@users.noreply.github.com>
2021-01-28 02:50:16 -0800

    Add files via upload

---
+++ float1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(int argc, char const *argv[])
+{
+    int a=400;
+    float b=30.50;
+    float c;
+    c=a+b;
+    printf("\n the sum of number is %f",c);
+    return 0;
+}
