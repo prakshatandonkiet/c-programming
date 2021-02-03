9f506fc74fbf058b49d5d659a121e9b2c8800560
prakshatandonkiet <75250776+prakshatandonkiet@users.noreply.github.com>
2021-02-03 02:29:13 -0800

    Add files via upload

---
+++ larger.c
@@ -0,0 +1,12 @@
+/* To print the lager value*/
+#include<stdio.h>
+int main(int argc, char const *argv[])
+{
+    int a=30;
+    int b=40;
+    if(b>a)
+    {
+        printf("larger value=%d",b);
+    }
+    return 0;
+}
