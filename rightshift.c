467ec5bd1e2e47660a87f3af1082364538cccbc7
prakshatandonkiet <75250776+prakshatandonkiet@users.noreply.github.com>
2021-02-07 02:05:58 -0800

    Add files via upload

---
+++ rightshift.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(int argc, char const *argv[])
+{
+    int a=12;
+    printf("\n the original number is %d",a);
+    printf("\n the number is shifted one bit to the right is %d",a>>1);
+     printf("\n the number shifted two bits to th right is %d",a>>2);
+
+    return 0;
+}
