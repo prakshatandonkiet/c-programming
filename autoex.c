b276108e534a675b6f84e696439f1a4c998f5a9d
prakshatandonkiet <75250776+prakshatandonkiet@users.noreply.github.com>
2021-02-12 05:48:04 -0800

    Add files via upload

---
+++ autoex.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int num2;
+int main(int argc, char const *argv[])
+{
+    auto int num;
+    register int b;
+    printf("%d",num);
+    printf("%d\n",num2);
+    printf("%d\n",b);
+    return 0;
+}
